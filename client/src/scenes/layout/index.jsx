d3aa765ead77e6864e3e88c3c0c3cdda2e00bfd7
Prasham1710 <jainprasham17@gmail.com>
2023-03-12 05:15:45 +0530

    navbar completed

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import { Box, useMediaQuery} from "@mui/material";
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Navbar from 'components/Navbar';
+
+const Layout = () => {
+    const isNonMobile = useMediaQuery("(min-width :600px)") ;
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  return ( 
+    <Box width="100%" height="100%">
+       <Box>
+        <Navbar/>
+        <Outlet />
+    </Box> 
+    </Box>
+  )
+}
+
+export default Layout;
